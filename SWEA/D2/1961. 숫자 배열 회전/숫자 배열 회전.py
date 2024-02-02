3eff3d3b7b613209bdcd8fe777526520e8601289
jun_grammer <143869498+hwangprogram@users.noreply.github.com>
2024-02-02 15:02:10 +0900

    [D2] Title: 숫자 배열 회전, Time: 147 ms, Memory: 58,552 KB -BaekjoonHub

--- SWEA/D2/1961. 숫자 배열 회전/숫자 배열 회전.py
+++ SWEA/D2/1961. 숫자 배열 회전/숫자 배열 회전.py
@@ -1,30 +1,37 @@
-def rotate(N, arr):
-    lst = [list(tp) for tp in zip(*arr)]
-    for i in range(N):
-        lst[i].reverse()
-    return lst
-
-def to_string(N, lst):
-    st = ''
-    for i in range(N):
-        st += str(lst[i])
-    return st
-
-# 입력
 T = int(input())
 
+# 전치해주고 뒤집어주는 함수 rotate
+def rotate(n, arr):
+    lst = [list(tp) for tp in zip(*arr)]
+
+    for i in range(n):
+        lst[i].reverse()
+
+    for i in range(n):
+        for j in range(n):
+            lst[i][j] = str(lst[i][j])
+
+    return lst
+
+
 for tc in range(1, T + 1):
+    # 입력
+    # N x N 행렬
     N = int(input())
 
-    arr = [list(map(int, input().split())) for _ in range(N)]
+    # 행렬 mat
+    mat = [list(map(int, input().split())) for _ in range(N)]
 
+    # 논리
+    # 돌려주고 뒤집어주는 함수 rotate 정의
+
+    orginal_mat = mat
+
+    # 출력
     print(f'#{tc}')
     for i in range(N):
-        ans_90 = rotate(N, arr)
-        ans_180 = rotate(N, ans_90)
-        ans_270 = rotate(N, ans_180)
-        s_9 = to_string(N, ans_90[i])
-        s_18 = to_string(N, ans_180[i])
-        s_27 = to_string(N, ans_270[i])
-
-        print(s_9, s_18, s_27)
+        for j in range(3):
+            mat = rotate(N, mat)
+            print(''.join(mat[i]), end=' ')
+        mat = orginal_mat
+        print()

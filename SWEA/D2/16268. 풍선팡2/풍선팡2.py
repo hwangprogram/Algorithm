5af6173ab560b1a23f3d193a986f06f3171d7744
jun_grammer <143869498+hwangprogram@users.noreply.github.com>
2024-02-02 13:54:03 +0900

    [D2] Title: 풍선팡2, Time: 200 ms, Memory: 62,784 KB -BaekjoonHub

--- SWEA/D2/16268. 풍선팡2/풍선팡2.py
+++ SWEA/D2/16268. 풍선팡2/풍선팡2.py
@@ -1,34 +1,33 @@
 T = int(input())
 
 for tc in range(1, T + 1):
-    # 입력
-    # N개의 행, M개의 열
-    # 입력받는 2차원 배열 lst
+    #  입력
+    # N x M 개의 풍선
     N, M = map(int, input().split())
-    lst = [list(map(int, input().split())) for _ in range(N)]
+
+    # 풍선들 balloons
+    balloons = [list(map(int, input().split())) for _ in range(N)]
 
     # 로직
-    # 상하좌우 델타값을 입력 di, dj
-    # 2차원 리스트를 순회하며 값 더함
-    # 합을 입력받는 변수 sum_v
-    # 그중 최댓값을 입력받는 변수 mx_v
-    di = [0, 1, 0, -1, 0]
-    dj = [0, 0, 1, 0, -1]
+    # 추가로 터트려줄 풍선들의 델타값
+    di = [0, 1, 0, -1]
+    dj = [1, 0, -1, 0]
+
+    # 합 sum_v, 최댓값 mx_v
     sum_v = mx_v = 0
 
+    # 풍선 순회
     for i in range(N):
         for j in range(M):
             sum_v = 0
-            for k in range(5):
+            sum_v += balloons[i][j]
+            for k in range(4):
                 ni = i + di[k]
                 nj = j + dj[k]
-								# 인덱스 에러 방지
-                if 0 <= ni < N and 0 <= nj < M:
-                    sum_v += lst[ni][nj]
-						# mx_v값이 sum_v값보다 작다면 mx_v를 sum_v값으로 치환 (최대값 구하기)
+                if ni >= 0 and ni < N and nj >= 0 and nj < M:
+                    sum_v += balloons[ni][nj]
             if mx_v < sum_v:
                 mx_v = sum_v
-
 
     # 출력
     print(f'#{tc} {mx_v}')

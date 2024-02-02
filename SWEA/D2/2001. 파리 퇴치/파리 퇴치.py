f0e872c071c2fb6b5e7fb5924af7c4810136ab4e
jun_grammer <143869498+hwangprogram@users.noreply.github.com>
2024-02-02 15:35:50 +0900

    [D2] Title: 파리 퇴치, Time: 169 ms, Memory: 59,028 KB -BaekjoonHub

--- SWEA/D2/2001. 파리 퇴치/파리 퇴치.py
+++ SWEA/D2/2001. 파리 퇴치/파리 퇴치.py
@@ -2,24 +2,27 @@ T = int(input())
 
 for tc in range(1, T + 1):
     # 입력
-    # N x N 의 배열
-    # M x M 의 크기를 가지는 파리채
+    # 배열의 한변의 길이 N
+    # 파리채 한변 길이 M
     N, M = map(int, input().split())
-    # N x N 크기를 가지는 배열 lst
-    lst = [list(map(int, input().split())) for _ in range(N)]
+
+    # 배열 arr
+    arr = [list(map(int, input().split())) for _ in range(N)]
 
     # 로직
-    # M x M 크기의 배열이 N x N 배열의 내부를 순회하며 그 합을 카운트하는 변수 s_v
-    # 카운트 된 값 중 최댓값을 가려내기 위한 변수 mx_v
-    # M x M 크기의 파리채
-    mx_v = s_v = 0
-    for i in range(N - M + 1):
-        for j in range(N - M + 1):
-            s_v = 0
-            for m in range(M):
-                for n in range(M):
-                    s_v += lst[i + m][j + n]
-            if mx_v < s_v:
-                mx_v = s_v
+    # 배열을 순회
+    # 배열 내부에서 파리채의 면적만큼 순회하며 퇴치한 파리수 더하기
+    # 합 변수 sum_v, 최대 합 mx_sum
+    sum_v = mx_sum = 0
 
-    print(f'#{tc} {mx_v}')
+    for i in range(N):
+        for j in range(N):
+            sum_v = 0
+            for k in range(M):
+                for l in range(M):
+                    if i + k < N and j + l < N:
+                        sum_v += arr[i + k][j + l]
+            mx_sum = max(mx_sum, sum_v)
+
+    # 출력
+    print(f'#{tc} {mx_sum}')

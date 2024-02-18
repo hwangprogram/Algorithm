419166ffadd338ee2cd67e204b4d5edfa3cc64a8
jun_grammer <143869498+hwangprogram@users.noreply.github.com>
2024-02-18 14:01:45 +0900

    [D3] Title: 삼성시의 버스 노선, Time: 253 ms, Memory: 63,824 KB -BaekjoonHub

--- SWEA/D3/6485. 삼성시의 버스 노선/삼성시의 버스 노선.py
+++ SWEA/D3/6485. 삼성시의 버스 노선/삼성시의 버스 노선.py
@@ -1,31 +1,19 @@
 T = int(input())
 
-for tc in range(1, T + 1):
-    # 입력
-    # 버스노선의 수 N
-    N = int(input())
+for tc in range(1, T+1):
+    N = int(input())    # N개의 노선
 
-    # 각 버스가 지나가는 정류장 : A ~ B
-    AB_lst = []
+    bus_stop = [0] * 5001   # 5000개의 버스 정류장
+    for n in range(N):
+        A, B = map(int, input().split())    # A, B 사이를 지나는 노선
 
-    for _ in range(N):
-        AB_lst.append(list(map(int, input().split())))
+        for i in range(A, B+1):
+            bus_stop[i] += 1                # A, B가 지나는 정류장들을 카운트
 
-    # P개의 버스 정류장
-    P = int(input())
+    P = int(input())    # P개의 정류장들
 
-    # 각 정거장 리스트
-    station = [0] * P
-
-    # 각 정류장 C
-    for j in range(P):
+    print(f'#{tc}', end=' ')
+    for p in range(P):
         C = int(input())
-        for i in AB_lst:
-            if C >= i[0] and C <= i[1]:
-                station[j] += 1
-
-    # 로직
-    # C를 입력 받으며 A 보다 크고, B보다 작다면 C를 지나가는 리스트 station에 append
-
-    # 출력
-    print(f'#{tc}', *station)
+        print(bus_stop[C], end=' ')
+    print()

d783b8a2e6d5aa7d552f58ceaeeea6f2519570eb
jun_grammer <143869498+hwangprogram@users.noreply.github.com>
2024-02-20 12:01:03 +0900

    [D3] Title: 오목 판정, Time: 234 ms, Memory: 63,024 KB -BaekjoonHub

--- SWEA/D3/11315. 오목 판정/오목 판정.py
+++ SWEA/D3/11315. 오목 판정/오목 판정.py
@@ -1,29 +1,38 @@
-dt = ((1, 0), (-1, 0), (0, 1), (0, -1), (1, 1), (1, -1), (-1, 1), (-1, -1))  # 8방향 탐색
+dt = ((0, 1), (1, -1), (1, 0), (1, 1))  # 탐색할 4방향 (우, 좌하, 하, 우하)
 
+def find(x, y, arr):        # 오목판정 함수
+    for _x, _y in dt:       # 4방향 확인
+        cnt = 1             # 2차원 리스트를 순회하며 돌을 찾은 것이므로, cnt를 1로 초기화 하고 시작
+        for t in range(1, 5):   # 4칸 이동하며 탐색 -> 5목인지만 판정하면 되므로
+            nx, ny = x + _x * t, y + _y * t # 이동할 좌표
 
-def search(arr):
-    for x in range(N):  # 오목판 순회
-        for y in range(N):
-            if arr[x][y] == 'o':  # o를 찾으면 탐색 시작
-                for _x, _y in dt:  # 우선 방향 선택
-                    cnt = 1
-                    for k in range(1, 5):  # 5번 이동하며 탐색
-                        nx, ny = x + _x * k, y + _y * k
-
-                        if 0 <= nx < N and 0 <= ny < N and arr[nx][ny] == 'o':  # 범위 내에 있고 찾는 값이라면
-                            cnt += 1
-                            if cnt == 5:  # 돌이 5개 이상 연속하면
-                                return 'YES'
-    else:
-        return 'NO'
+            if 0 <= nx < N and 0 <= ny < N and arr[nx][ny] == 'o':      # 범위 내에 있고, 돌이 있다면,
+                cnt += 1    # 카운트
+                if cnt == 5:    # 오목을 찾았다면,
+                    return 1
+    else:               # 못 찾았다면 (for문을 다 돌았지만 찾지 못했다면)
+        return 0
 
 
 T = int(input())
 
-for tc in range(1, T + 1):
-    N = int(input())  # 오목판의 크기 NxN
-    rock = [list(input()) for _ in range(N)]  # 오목판
+for tc in range(1, T+1):
+    N = int(input())    # NxN 크기의 판
 
-    cnt = 0
+    stage = [list(input()) for _ in range(N)]   # 판
 
-    print(f'#{tc} {search(rock)}')
+    ans = 'NO'  # 찾았나 못찾았나
+    escape = 0  # 탈출
+
+    print(f'#{tc}', end=' ')
+    for i in range(N):
+        for j in range(N):          # 판 순회
+            if stage[i][j] == 'o':  # 돌을 찾았을 때부터 탐색 시작
+                if find(i, j, stage) == 1:  # 오목을 찾았으면,
+                    ans = 'YES'
+                    escape = 1
+                    break
+        if escape == 1:
+            break
+
+    print(ans)

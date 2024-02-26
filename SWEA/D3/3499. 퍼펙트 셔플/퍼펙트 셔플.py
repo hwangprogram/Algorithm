18cc4a1588fe26e390a024adef001f2a13e92dc6
jun_grammer <143869498+hwangprogram@users.noreply.github.com>
2024-02-26 17:13:15 +0900

    [D3] Title: 퍼펙트 셔플, Time: 150 ms, Memory: 58,584 KB -BaekjoonHub

--- SWEA/D3/3499. 퍼펙트 셔플/퍼펙트 셔플.py
+++ SWEA/D3/3499. 퍼펙트 셔플/퍼펙트 셔플.py
@@ -1,22 +1,41 @@
+'''
+SWEA 퍼펙트 셔플
+
+퍼펙트 셔플 : 카드 덱을 정확히 절반으로 나누고, 나눈 것들에서 교대로 카드를 뽑아 새로운 덱을
+만드는 것
+카드가 주어질 때, 퍼펙트 셔플하면 어떤 순서가 되는지 출력하라
+
+설계
+투 포인트 알고리즘
+a = 맨 처음
+b = 중간 + 1
+
+Loop {
+    a 출력 후 a + 1
+    b 출력 후 b + 1
+}
+'''
 T = int(input())
+N = 0
+arr = []
 
-for tc in range(1, T + 1):
+
+def get_result():
+    a = 0
+    b = (len(arr) + 1) // 2
+
+    for turn in range(len(arr)):
+        if turn % 2 == 0:
+            print(arr[a], end=' ')
+            a += 1
+        else:
+            print(arr[b], end=' ')
+            b += 1
+
+
+for tc in range(1, T+1):
     N = int(input())
-
-    card = list(input().split())
-
-    if N % 2 == 0:
-        sep_lst1 = card[:N // 2]
-        sep_lst2 = card[N // 2:]
-    else:
-        sep_lst1 = card[:N // 2 + 1]
-        sep_lst2 = card[N // 2 + 1:]
-
+    arr = list(map(str, input().split()))
     print(f'#{tc}', end=' ')
-
-    for i in range(len(sep_lst1)):  # 둘 중 길이가 긴 리스트의 길이만큼
-        print(sep_lst1.pop(0), end=' ')
-
-        if sep_lst2:    # 리스트가 비어 있지 않다면
-            print(sep_lst2.pop(0), end=' ')
+    get_result()
     print()

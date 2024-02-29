5cf32301818af452ce0224cb37b2aabd2d4086d6
jun_grammer <143869498+hwangprogram@users.noreply.github.com>
2024-02-29 12:06:47 +0900

    [D4] Title: 쇠막대기 자르기, Time: 1,887 ms, Memory: 90,352 KB -BaekjoonHub

--- SWEA/D4/5432. 쇠막대기 자르기/쇠막대기 자르기.py
+++ SWEA/D4/5432. 쇠막대기 자르기/쇠막대기 자르기.py
@@ -1,34 +1,40 @@
-T = int(input())
+'''
+SWEA 쇠막대기 자르기
+
+문제 요약:
+()(((()())(())()))(())
+이런식으로 주어졌을 때, () -> 바로 열고 닫긴 소괄호 : 레이저
+그 외의 것들은 쇠막대기이다.
+레이저로 절단한 후의 쇠막대기 수를 구하라
+
+전략:
+1. ()를 l로 바꿔준다 (lazer)
+2. ( 이 나올때마다 stack에 쌓아주고, 'l'이 나올때 마다 카운트 해준 다음, )가 나오면 (을
+pop해주면서 레이저카운트 + 1 만큼을 합에 더해줌
+'''
+T = int(input().rstrip())
 
 for tc in range(1, T+1):
-    # 쇠막대기 수열 입력
-    steel = input()
+    steel = input().rstrip()
 
-    steel = steel.replace('()', 'r')
+    steel = steel.replace('()', 'l')    # ()를 l로 바꿔준다.
 
-    # 쇠막대기 or 레이저 값을 쌓을 스택
-    stack = ''
+    stack = []
+    lazer_cnt = 0
+    sm = 0
+    '''
+    lazer_cnt를 사용
+    lazer_cnt는 스택에 쌓여있는 (과 )사이의 레이저 값
+    '''
+    cnt = 0
+    for s in steel:
+        if s == '(':    # (을 만나면 스택에 append하고, 얼마나 쌓였는지 체크함
+            sm += 1
+            stack.append(s)
+        elif s == 'l':  # l을 만나면 자름 (자를 때 마다 현재 스택에 쌓인 괄호 수만큼 더해줌(등분)
+            cnt = len(stack)
+            sm += cnt
+        elif s == ')':
+            stack.pop()
 
-    # 레이저를 세어줄 카운트 lazer
-    lazer = 0
-
-    # 쇠막대기(잘리기 전) 의 갯수
-    og_bar = 0
-
-    # 쇄막대기 갯수 bars
-    bars = 0
-
-    for i in range(len(steel)):
-        # ( 라면 스택에 push
-        if steel[i] == '(':
-            stack += '('
-        elif steel[i] == 'r':
-            stack += 'r'
-        # ) 를 만나게 되면 stack의 마지막에 있는 ( 과 ) 사이에
-        # 몇개의 r이 있는지 세고, bars에 r + 1을 plus 함
-        elif steel[i] == ')':
-            cnt = stack[stack.rindex('('):].count('r')
-            stack = stack[:stack.rindex('(')] + stack[stack.rindex('(') + 1:]
-            bars += cnt + 1
-
-    print(f'#{tc} {bars}')
+    print(f'#{tc} {sm}')

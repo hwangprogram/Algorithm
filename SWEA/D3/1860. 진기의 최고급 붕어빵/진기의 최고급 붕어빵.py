9db8e22f51bddadf19e4baf931ebd8e5a7a6f6eb
jun_grammer <143869498+hwangprogram@users.noreply.github.com>
2024-02-29 10:31:03 +0900

    [D3] Title: 진기의 최고급 붕어빵, Time: 554 ms, Memory: 64,908 KB -BaekjoonHub

--- SWEA/D3/1860. 진기의 최고급 붕어빵/진기의 최고급 붕어빵.py
+++ SWEA/D3/1860. 진기의 최고급 붕어빵/진기의 최고급 붕어빵.py
@@ -1,46 +1,25 @@
 T = int(input())
 
 for tc in range(1, T+1):
-    # N명의 사람, M초마다 K개의 붕어빵 만듦
+    # N 명의 사람, M초의 시간에 K개의 붕어빵 만듦
     N, M, K = map(int, input().split())
 
-    # 도착하는 사람들
-    arrive_ppl = list(map(int, input().split()))
+    '''
+    M초마다 K개의 붕어빵
+    그렇다면 X초에 붕어빵의 갯수는? X // M * K
+    '''
+    peoples = list(map(int, input().split()))
+    peoples.sort()
+    second = 0
 
-    # 정렬
-    arrive_ppl.sort()
-
-    arrive_ppl.append(0)
-
-    # 초별 붕어빵 변수 (증가함)
-    dorayakki = 0
-
-    # 붕어빵 조달 가능한가
     ans = 'Possible'
-
-    i = 0
-    for j in range(max(arrive_ppl) + 1):
-        # M의 배수 초 마다 붕어빵 만들기
-        if j % M == 0:
-            if j == 0:
-                continue
-            else:
-                dorayakki += K
-
-        if arrive_ppl[i] == 0:
+    sold_bread = 0
+    for ppl in peoples:
+        made_bread = (ppl // M) * K  # 지금까지 만든 빵의 갯수
+        sold_bread += 1     # 팔린 빵 갯수 누적
+        remain_bread = made_bread - sold_bread      # 재고
+        if remain_bread < 0:
             ans = 'Impossible'
-            break
-
-        # 사람이 올때마다 붕어빵 내어줌
-        while j == arrive_ppl[i]:
-            if dorayakki < 1:
-                ans = 'Impossible'
-                break
-            else:
-                dorayakki -= 1
-            i += 1
-
-        if ans == 'Impossible':
             break
 
     print(f'#{tc} {ans}')

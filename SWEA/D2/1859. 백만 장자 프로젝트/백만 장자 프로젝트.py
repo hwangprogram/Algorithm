8240198d51a37c3dbbef52032317b462fd2be3f0
jun_grammer <143869498+hwangprogram@users.noreply.github.com>
2024-02-29 14:14:03 +0900

    [D2] Title: 백만 장자 프로젝트, Time: 1,609 ms, Memory: 246,112 KB -BaekjoonHub

--- SWEA/D2/1859. 백만 장자 프로젝트/백만 장자 프로젝트.py
+++ SWEA/D2/1859. 백만 장자 프로젝트/백만 장자 프로젝트.py
@@ -1,18 +1,29 @@
-T = int(input())
+'''
+SWEA 백만장자 프로젝트
 
-for tc in range(1, T + 1):
-    # 입력
-    # N개의 가격 cost
-    N = int(input())
-    cost = list(map(int, input().split()))
+문제 요약:
+1. 원재는 연속된 N일 동안의 물건의 매매가를 예측하여 알고 있다.
+2. 당국의 감시망에 걸리지 않기 위해 하루에 최대 1만큼 구입할 수 있다.
+3. 판매는 얼마든지 할 수 있다.
 
-    # 판매액 합
-    sum_cost = 0
+전략:
+1. 뒤에서부터 순회하기 시작
+2. 순회하면서 스택에 나보다 큰 값을 넣는다
+3. 뒤에 있는 값이 나보다 작으면 판다.
+4. 뒤에 있는 값이 나보다 크면 그 값으로 교체한다.
+'''
+T = int(input().rstrip())
 
-    if cost.index(max(cost)) != 0:
-         while cost != []:
-            sell_idx = cost.index(max(cost))
-            sum_cost += max(cost) * len(cost[:sell_idx]) - sum(cost[:sell_idx])
-            cost[:sell_idx + 1] = []
+for tc in range(1, T+1):
+    N = int(input().rstrip())
+    N_lst = list(map(int, input().rstrip().split()))   # N개의 매매가
 
-    print(f'#{tc} {sum_cost}')
+    mx_n = 0
+    sm_n = 0
+    for n in range(N-1, -1, -1):
+        if N_lst[n] > mx_n:
+            mx_n = N_lst[n]
+        else:
+            sm_n += (mx_n - N_lst[n])
+
+    print(f'#{tc} {sm_n}')

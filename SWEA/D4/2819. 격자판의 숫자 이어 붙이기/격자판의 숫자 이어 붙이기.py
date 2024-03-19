3f643853f3985bbae19afd27ce17f277725d8a65
jun_grammer <143869498+hwangprogram@users.noreply.github.com>
2024-03-19 23:19:33 +0900

    [D4] Title: 격자판의 숫자 이어 붙이기, Time: 423 ms, Memory: 77,420 KB -BaekjoonHub

--- SWEA/D4/2819. 격자판의 숫자 이어 붙이기/격자판의 숫자 이어 붙이기.py
+++ SWEA/D4/2819. 격자판의 숫자 이어 붙이기/격자판의 숫자 이어 붙이기.py
@@ -1,8 +1,8 @@
 dt = ((1, 0), (-1, 0), (0, 1), (0, -1))
-def dfs(i, j, path):
+def dfs(i, j):
     # 기저조건 : path의 길이가 7이 되면 종료
     if len(path) == 7:
-        path_set.add(path)
+        path_set.add(str(path[:]))
         return
 
     # 재귀조건 : 방문 여부 상관 x 동서남북으로 이동
@@ -11,7 +11,9 @@ def dfs(i, j, path):
         nj = j + dj
 
         if 0 <= ni < 4 and 0 <= nj < 4:
-            dfs(ni, nj, path+str(grid[ni][nj]))
+            path.append(grid[ni][nj])
+            dfs(ni, nj)
+            path.pop()
 
 
 T = int(input())
@@ -20,8 +22,9 @@ for tc in range(1, T+1):
     # 격자판
     grid = [list(map(int, input().split())) for _ in range(4)]
     path_set = set()
+    path = []
     for i in range(4):
         for j in range(4):
-            dfs(i, j, str(grid[i][j]))
+            dfs(i, j)
 
     print(f"#{tc} {len(path_set)}")

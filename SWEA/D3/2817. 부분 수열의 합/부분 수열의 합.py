1fc78f457562bed689d5c8b7d5e300a945f12c3c
jun_grammer <143869498+hwangprogram@users.noreply.github.com>
2024-03-24 23:16:06 +0900

    [D3] Title: 부분 수열의 합, Time: 7,139 ms, Memory: 63,564 KB -BaekjoonHub

--- SWEA/D3/2817. 부분 수열의 합/부분 수열의 합.py
+++ SWEA/D3/2817. 부분 수열의 합/부분 수열의 합.py
@@ -1,31 +1,30 @@
-path = []
-def sub_set(n):
-    # 기저조건
-    if n == N:
-        print(*path)
+def make_subset(idx, sm):
+    global cnt
+
+    # 가지치기: 합이 K초과 되면 종료
+    if sm > K:
         return
 
-    for i in range(N):
-        path.append(N_lst[i])
-        sub_set(n+1)
-        path.pop()
+    # 기저조건: idx가 N이상 되면 종료
+    if idx == N:
+        # 정답조건: sm이 K이면 정답처리
+        if sm == K:
+            cnt += 1
+        return
+
+    # 재귀조건: sm에 다음 원소를 더하는가, 더하지 않는가
+    # 더한다.
+    make_subset(idx+1, sm+A[idx])
+    # 더하지 않는다
+    make_subset(idx+1, sm)
 
 
 T = int(input())
 
 for tc in range(1, T+1):
     N, K = map(int, input().split())
-    N_lst = list(map(int, input().split()))
+    A = list(map(int, input().split()))
 
     cnt = 0
-    set_lst = []
-    for i in range(1<<N):
-        sum_v = 0
-        for j in range(N):
-            if i & (1<<j):
-                sum_v += N_lst[j]
-
-        if sum_v == K:
-            cnt += 1
-
-    print(f'#{tc} {cnt}')
+    make_subset(0, 0)
+    print(f"#{tc} {cnt}")

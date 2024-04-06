506482380668c4128b940eaa63329aac3a33147b
jun_grammer <143869498+hwangprogram@users.noreply.github.com>
2024-04-06 14:22:21 +0900

    [Unrated] Title: [모의 SW 역량테스트] 수영장, Time: 234 ms, Memory: 72,684 KB -BaekjoonHub

--- SWEA/Unrated/1952. ［모의 SW 역량테스트］ 수영장/［모의 SW 역량테스트］ 수영장.py
+++ SWEA/Unrated/1952. ［모의 SW 역량테스트］ 수영장/［모의 SW 역량테스트］ 수영장.py
@@ -1,42 +1,40 @@
-# 깊이 우선 탐색: 1일권, 1달권, 3달권, 1년권
-def dfs(idx, cost):
+'''
+전략:
+이용계획 리스트를 방문 배열로 사용한다.
+dfs로 1일권, 1달권, 3달권, 1년권 사용하는 경우를 구분한다.
+그 중 최소값을 찾는다.
+'''
+
+def dfs(cnt, cost):
     global min_cost
 
-    # 가지치기 : min_cost 이상이면 return
-    if cost >= min_cost:
-        return
-
-    # 기저조건 : 12월 초과되면 return
-    if idx == 13:
-        # 정답조건 : 가장 작은 cost 값을 return
+    # 기저조건: 12월까지 진행하고 종료
+    if cnt == 12:
+        # 정답처리: 최소값 구하기
         min_cost = min(min_cost, cost)
         return
 
-    # 재귀조건 : 1일, 1달, 3달, 1년(12달) 차례로 탐색 시작
-    # 1일
-    dfs(idx+1, cost+(fee[0]*plan[idx-1]))
-
-    # 1달
-    dfs(idx+1, cost+fee[1])
-
-    # 3달
-    # 인덱스 초과 방지 10월 전까지
-    if idx <= 10:
-        dfs(idx+3, cost+fee[2])
-
-    # 1년
-    # 인덱스 초과 방지 1월만
-    if idx == 1:
-        dfs(idx+12, cost+fee[3])
+    # 재귀조건: 각 이용권 사용하는 경우의 수
+    # 1일권
+    dfs(cnt + 1, cost + d * plan[cnt])
+    # 1달권
+    dfs(cnt + 1, cost + m)
+    # 3달권: 10월까지만 사용가능
+    if cnt < 10:
+        dfs(cnt + 3, cost + m_3)
+    # 1년권: 1월에만 사용가능
+    if cnt == 0:
+        dfs(cnt + 12, cost + y)
 
 
 T = int(input())
 
 for tc in range(1, T+1):
-    fee = list(map(int, input().split()))
+    # 1일, 1달, 3달, 1년 이용권 요금
+    d, m, m_3, y = map(int, input().split())
+    # 이용계획
     plan = list(map(int, input().split()))
-
-    min_cost = 3000*12
-
-    dfs(1, 0)
-    print(f"#{tc} {min_cost}")
+    # 최소비용
+    min_cost = float('inf')
+    dfs(0, 0)
+    print(f'#{tc} {min_cost}')

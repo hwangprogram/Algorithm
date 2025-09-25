0a33d8538b90570749211153a3c8802852e134b6
jun_grammer <143869498+hwangprogram@users.noreply.github.com>
2025-09-25 22:13:36 +0900

    [Platinum V] Title: 버블 소트, Time: 104 ms, Memory: 5932 KB -BaekjoonHub

---
+++ 백준/Platinum/1517. 버블 소트/버블 소트.cc
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+void merge_sort(int s, int e);
+static vector<int> A;
+static vector<int> tmp;
+static long result;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int N;
+    cin >> N;
+    A = vector<int>(N+1, 0);
+    tmp = vector<int>(N+1, 0);
+
+    for (int i = 1; i <= N; i++) {
+        cin >> A[i];
+    }
+    result = 0;
+    merge_sort(1, N);   // 병합 정렬 수행하기
+    cout << result << '\n';
+    return 0;
+}
+
+void merge_sort(int s, int e) {
+    if (e - s < 1)
+        return;
+
+    int m = s + (e - s) / 2;
+    // 재귀 함수 형태로 구현
+    merge_sort(s, m);
+    merge_sort(m+1, e);
+
+    for (int i = s; i <= e; i++) {
+        tmp[i] = A[i];
+    }
+
+    int k = s;
+    int index1 = s;
+    int index2 = m+1;
+
+    while (index1 <= m && index2 <= e) {    // 두 그룹을 병합하는 로직
+        if (tmp[index1] > tmp[index2]) {
+            A[k] = tmp[index2];
+            // 뒤쪽 데이터 값이 작아 선택되는 경우 결과값 업데이트
+            result = result + index2 - k;
+            k++;
+            index2++;
+        }
+        else{
+            A[k] = tmp[index1];
+            k++;
+            index1++;
+        }
+    }
+
+    // 한쪽 그룹이 모두 선택된 후 남아있는 값 정리하기
+    while (index1 <= m) {
+        A[k] = tmp[index1];
+        k++;
+        index1++;
+    }
+    while (index2 <= e) {
+        A[k] = tmp[index2];
+        k++;
+        index2++;
+    }
+}

fbc989eb0ed29755111f1367e19b5973f63ebf88
jun_grammer <143869498+hwangprogram@users.noreply.github.com>
2025-10-20 16:17:01 +0900

    [Gold V] Title: 집합의 표현, Time: 36 ms, Memory: 5932 KB -BaekjoonHub

---
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.cc
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+
+using namespace std;
+static vector<int> parent;
+void unionfunc(int a, int b);
+int find(int a);
+bool checkSame(int a, int b);
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int N, M;
+    cin >> N >> M;
+    parent.resize(N+1);
+
+    for (int i = 1; i <= N; ++i) {  // 대표 노드를 자기 자신으로 초기화
+        parent[i] = i;
+    }
+    for (int i = 0; i < M; ++i) {
+        int question, a, b;
+        cin >> question >> a >> b;
+
+        if (question == 0) {    // 집합 합치기
+            unionfunc(a, b);
+        }
+        else {                  // 같은 집합의 원소인지 확인하기
+            if (checkSame(a, b)) cout << "YES\n";
+            else cout << "NO\n";
+        }
+    }
+
+    return 0;
+}
+
+void unionfunc(int a, int b) {  // union 연산: 바로 연결이 아닌 대표 노드끼리 연결
+    a = find(a);
+    b = find(b);
+
+    if (a != b) {
+        parent[b] = a;
+    }
+}
+
+int find(int a) {   // find 연산: 대표 노드를 찾아서 반환
+    if (a == parent[a]) {
+        return a;
+    }
+    else {
+        return parent[a] = find(parent[a]); // 재귀함수 형태로 구현
+    }
+}
+
+bool checkSame(int a, int b) {  // 두 원소가 같은 집합인지 확인
+    a = find(a);
+    b = find(b);
+
+    if (a == b) {
+        return true;
+    }
+    return false;
+}
